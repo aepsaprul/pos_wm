9934e179e9f50ca651182bdca984f9b32c9355f8
aep <aepsaprul18@gmail.com>
2022-05-26 05:53:23 +0700

    redirect to dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,33 +1,7 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">Dashboard</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard</li>
-                    </ol>
-                </div>
-            </div>
-        </div>
-    </div>
+@if (Auth::user()->employee->shop->category == "toko")
+<script>window.location = "{{ URL::route('dashboard.shop') }}";</script>
+@else
+<script>window.location = "{{ URL::route('dashboard.index') }}";</script>
+@endif
 
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <h3 class="text-center"><img src="{{ asset('public/assets/icon.png') }}" alt="logo" style="width: 300px;"></h3>
-                </div>
-            </div>
-        </div>
-    </section>
-</div>
-@endsection
