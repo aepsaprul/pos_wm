2c8d69cb491bff38f2aa38fdfe305b9a6ee2b109
aep <aepsaprul18@gmail.com>
2022-05-26 08:29:15 +0700

    ketinggalan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 
 @if (Auth::user()->employee->shop->category == "toko")
-<script>window.location = "{{ URL::route('dashboard.shop') }}";</script>
+    <script>window.location = "{{ URL::route('dashboard.shop') }}";</script>
 @else
-<script>window.location = "{{ URL::route('dashboard.index') }}";</script>
+    <script>window.location = "{{ URL::route('dashboard.index') }}";</script>
 @endif
 
