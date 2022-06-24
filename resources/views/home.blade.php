682af267c9ac97c0cb9d0c77b8019ae3c37ea00d
aep <aepsaprul18@gmail.com>
2022-06-25 04:11:49 +0700

    if admin redirect to dashboard index

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,11 @@
-
-@if (Auth::user()->employee->shop->category == "toko")
-    <script>window.location = "{{ URL::route('dashboard.shop') }}";</script>
+@if (Auth::user()->employee)
+    @if (Auth::user()->employee->shop->category == "toko")
+        <script>window.location = "{{ URL::route('dashboard.shop') }}";</script>
+    @else
+        <script>window.location = "{{ URL::route('dashboard.index') }}";</script>
+    @endif
 @else
     <script>window.location = "{{ URL::route('dashboard.index') }}";</script>
 @endif
+
 
