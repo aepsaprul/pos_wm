50bb719018d679b3a3f55e14f572d7c235f4f1f7
aep <aepsaprul18@gmail.com>
2022-08-02 11:48:58 +0700

    ketinggalan

--- resources/views/pages/inventory_product_in/index.blade.php
+++ resources/views/pages/inventory_product_in/index.blade.php
@@ -19,12 +19,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Toko</h1>
+                    <h1 class="m-0">Produk Masuk</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Toko</li>
+                        <li class="breadcrumb-item active">Produk Masuk</li>
                     </ol>
                 </div>
             </div>
